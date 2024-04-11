59a12cf3852f1710dc39d742e04b287135bc2a45
mohamedArshad07 <parkarshad@gmail.com>
2024-04-11 10:37:52 +0200

    small changes

--- src/test/java/testRunner/runnerfile.java
+++ src/test/java/testRunner/runnerfile.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		features = "C:\\Users\\arshe\\eclipse-workspace\\ui.automation.cucumber2\\src\\test\\java\\feature\\Register.feature",
 		glue = {"stepDefinitions","hooks"},
-       	// tags = "@SmokeTest1",
+       	 tags = "@SmokeTest",
 		publish=true,
 		monochrome = true,
 		plugin={"pretty","html:target/CucumberReports/CucumberReport.html",
